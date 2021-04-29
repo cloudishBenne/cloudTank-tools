1f0273f812cefa7bcfe91dfef15a420b014547c9
cloudishBenne <30512375+cloudishBenne@users.noreply.github.com>
2021-04-29 20:50:40 +0200

    Update setup_pop-os_oibaf-graphics.sh
    fixed upgrade process - we have to make sure, the new packages are installed and not holded back

--- setup_pop-os_oibaf-graphics.sh
+++ setup_pop-os_oibaf-graphics.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
 # Keep repo on update enabled
 sudo cp pop-oibaf-graphics.cfg /etc/update-manager/release-upgrades.d/pop-oibaf-graphics.cfg
 
 # Pinning the repo over pop_os own packages
 sudo cp pop-oibaf-graphics /etc/apt/preferences.d/pop-oibaf-graphics
 
+
 # Adding the PPA
 sudo add-apt-repository ppa:oibaf/graphics-drivers
-sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get update && sudo apt upgrade -y --fix-broken --fix-missing --install-recommends
